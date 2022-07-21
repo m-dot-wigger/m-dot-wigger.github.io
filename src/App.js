dc63b8016f90e5b54dc98627c3769da85c9dfce2
sstoddart-vgw <samuel.stoddart@vgw.co>
2022-07-21 13:36:07 +0800

    Add CICD workshop template

--- src/App.js
+++ src/App.js
@@ -1,21 +1,18 @@
-import logo from './logo.svg';
+import bomb from './bomb.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <img src={bomb} className="App-logo" alt="bomb" />
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.youtube.com/watch?v=oHg5SJYRHA0"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Who needs automation! Manual deploys for the win!
         </a>
       </header>
     </div>
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,12 @@
 import { render, screen } from '@testing-library/react';
 import App from './App';
 
-test('renders learn react link', () => {
+// This test checks to see that a hyperlink is rendered with the expected url and text values
+test('renders link', () => {
   render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  const expectedLinkText = 'From local source code to fully tested and deployed application in a single push';
+  const expectedLinkUrl = 'https://www.redhat.com/en/topics/devops/what-is-ci-cd'
+  const linkElement = screen.getByText(expectedLinkText);
   expect(linkElement).toBeInTheDocument();
+  expect(linkElement.closest('a')).toHaveAttribute('href', expectedLinkUrl);
 });
